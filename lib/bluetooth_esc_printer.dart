61ba5a36d75ab48b5ba6520a2e15ab87e37d273a
Houman Masnavi <info@drmasnavi.ir>
2025-07-21 10:40:43 +0330

    flutterprinter

---
+++ lib/bluetooth_esc_printer.dart
@@ -0,0 +1,16 @@
+/*
+ * bluetooth_esc_printer
+ * Created by Hooman Masnavi (senatormas)
+ * 
+ * Forked from esc_pos_bluetooth_plus
+ * Copyright (c) 2025. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+library bluetooth_esc_printer;
+
+export './src/enums.dart';
+export './src/services/flutter_blue_service.dart';
+export './src/services/esc_pos_printer_service_impl.dart';
+export './src/util/platform_util.dart';
+export './src/printer_manager.dart'; // if you implemented one
